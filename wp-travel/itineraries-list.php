3345f1bf501748bb810838bcd616ec6d9a821868
twindai <guandai@hotmail.com>
2024-10-30 09:04:20 +0100

    +1

--- wp-travel/itineraries-list.php
+++ wp-travel/itineraries-list.php
@@ -65,7 +65,7 @@ if ( isset( $wptravel_itineraries ) && ! empty( $wptravel_itineraries ) ) : ?>
 							<?php 
 								echo esc_attr( $wptravel_second_class ); 
 								echo 'wptravel_index_' . $wptravel_index;
-								if ( count( $wptravel_itineraries ) === $wptravel_index ) {
+								if ( count( $wptravel_itineraries )+1 === $wptravel_index ) {
 									echo ' wptravel_index_last';
 								}
 							?>
