dab361a973a125ebf46301a05fab50666533ea1b
twindai <guandai@hotmail.com>
2024-11-02 23:17:54 +0100

    redirect_account_booking

--- register_url.php
+++ register_url.php
@@ -1,6 +1,24 @@
 <?php
 // Add custom rewrite rule
-add_action('init', 'add_account_orders_rewrite');
-function add_account_orders_rewrite(){
-    add_rewrite_rule('^account/booking/?$', 'account/', 'top');
+add_action('init', 'redirect_account_booking');
+
+function redirect_account_booking() {
+    // Get the current URL path
+    $current_path = trim(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), '/');
+
+    // Check if the current path matches 'account/booking'
+    if ($current_path === 'account/booking') {
+        // Get the query string
+        $query_string = $_SERVER['QUERY_STRING'];
+
+        // Build the redirect URL
+        $redirect_url = '/account/';
+        if ($query_string) {
+            $redirect_url .= '?' . $query_string;
+        }
+
+        // Perform the redirect
+        header("Location: $redirect_url", true, 301);
+        exit();
+    }
 }
