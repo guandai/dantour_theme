ef76bd95e5f36ae74d901693848976619a2fa9f1
twindai <guandai@hotmail.com>
2024-11-03 00:22:13 +0100

    remove redirecta

--- register_url.php
+++ register_url.php
@@ -1,24 +1,24 @@
 <?php
-// Add custom rewrite rule
-add_action('init', 'redirect_account_booking');
+// // Add custom rewrite rule
+// add_action('init', 'redirect_account_booking');
 
-function redirect_account_booking() {
-    // Get the current URL path
-    $current_path = trim(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), '/');
+// function redirect_account_booking() {
+//     // Get the current URL path
+//     $current_path = trim(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), '/');
 
-    // Check if the current path matches 'account/booking'
-    if ($current_path === 'account/booking') {
-        // Get the query string
-        $query_string = $_SERVER['QUERY_STRING'];
+//     // Check if the current path matches 'account/booking'
+//     if ($current_path === 'account/booking') {
+//         // Get the query string
+//         $query_string = $_SERVER['QUERY_STRING'];
 
-        // Build the redirect URL
-        $redirect_url = '/account/';
-        if ($query_string) {
-            $redirect_url .= '?booking=1&' . $query_string;
-        }
+//         // Build the redirect URL
+//         $redirect_url = '/account/';
+//         if ($query_string) {
+//             $redirect_url .= '?booking=1&' . $query_string;
+//         }
 
-        // Perform the redirect
-        header("Location: $redirect_url", true, 301);
-        exit();
-    }
-}
+//         // Perform the redirect
+//         header("Location: $redirect_url", true, 301);
+//         exit();
+//     }
+// }
