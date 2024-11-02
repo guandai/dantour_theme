d21e335150837179828b6001ae41f0a3bee29732
twindai <guandai@hotmail.com>
2024-11-02 22:02:13 +0100

    h2

---
+++ load_css.php
@@ -0,0 +1,26 @@
+<?php
+// Exit if accessed directly
+if ( !defined( 'ABSPATH' ) ) exit;
+
+/**
+ * Function to enqueue CSS files conditionally based on the URL.
+ *
+ * @param string $file_name The name of the CSS file (without the .css extension).
+ * @param string $path_contains The substring to look for in the URL.
+ */
+function insert_css_by_url_name($file_name, $path_contains) {
+    add_action('wp_enqueue_scripts', function() use ($file_name, $path_contains) {
+        if (strpos($_SERVER['REQUEST_URI'], $path_contains) !== false) {
+            wp_enqueue_style(
+                $file_name . '_style',
+                get_stylesheet_directory_uri() . '/css/' . $file_name . '.css',
+                array(), // Dependencies
+                '1.0',    // Version
+                'all'     // Media
+            );
+        }
+    });
+}
+
+// Example usage:
+insert_css_by_url_name('wp-travel-dashboard', '/wp-travel-dashboard');
