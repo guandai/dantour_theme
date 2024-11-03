0cd59ffb4042dbd5904426080d8b3568e9984440
twindai <guandai@hotmail.com>
2024-11-03 11:40:12 +0100

    fix url

--- js/itinerary_booking_btn.js
+++ js/itinerary_booking_btn.js
@@ -2,5 +2,8 @@ document.addEventListener('DOMContentLoaded', function () {
 	setTimeout(async () => {
 			// const btn = document.querySelectorAll('#wp-travel-one-page-checkout-enables button');
 			_wp_travel.dashboard_url = 'https://dantourbooking.com/account/';
+
+			const login_in_link = document.querySelectorAll('.ReactModalPortal .ReactModal__Overlay .ReactModal__Content.booknow-btn-modal a');
+			login_in_link.href = _wp_travel.dashboard_url;
 	}, 0);
 });
