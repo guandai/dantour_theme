d31604ac26c4c964e6a2c93062f74c4322878048
twindai <guandai@hotmail.com>
2024-11-03 11:43:41 +0100

    no

--- js/itinerary_booking_btn.js
+++ js/itinerary_booking_btn.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', function () {
 			// const btn = document.querySelectorAll('#wp-travel-one-page-checkout-enables button');
 			_wp_travel.dashboard_url = 'https://dantourbooking.com/account/';
 
-			const login_in_link = document.querySelectorAll('.ReactModalPortal .ReactModal__Overlay .ReactModal__Content.booknow-btn-modal a');
+			const login_in_link = document.querySelector('.ReactModalPortal .ReactModal__Overlay .ReactModal__Content.booknow-btn-modal a');
 			login_in_link.href =  `${_wp_travel.dashboard_url}?redirect_to=${window.location.href}`;
 	}, 0);
 });
