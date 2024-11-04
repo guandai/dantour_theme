9ed6bebfb78e60d9f6dab365627b91ac85f81980
twindai <guandai@hotmail.com>
2024-11-04 18:37:29 +0100

    update

--- overwrite_fn.php
+++ overwrite_fn.php
@@ -1,26 +1,26 @@
 <?php
-// functions.php
 
-// Check if the original class exists before extending it
+// Check if the original WP_Travel_Cart class exists before creating the custom class.
 if ( class_exists( 'WP_Travel_Cart' ) ) {
-
-    // Define a custom class that extends the original WP_Travel_Cart
-    class Custom_WP_Travel_Cart extends WP_Travel_Cart {
-
-        // Override the cart_empty_message method with custom functionality
-        public function cart_empty_message() {
-            $url = get_post_type_archive_link( WP_TRAVEL_POST_TYPE );
-            echo sprintf(
-                __( 'Your cart is empty please <a href="%s"> click here </a> to add trips.', 'wp-travel' ),
-                esc_url( $url )
-            );
+        // Define a custom class that extends WP_Travel_Cart.
+        class Custom_WP_Travel_Cart extends WP_Travel_Cart {
+    
+            // Override the cart_empty_message method with custom functionality.
+            public function cart_empty_message() {
+                $url = get_post_type_archive_link( WP_TRAVEL_POST_TYPE );
+                echo sprintf( __( 'Your cart is empty please <a href="%s"> click here </a> to add trips.', 'wp-travel' ), esc_url( $url ) ); // @phpcs:ignore
+            }
+        }
+    
+        // Replace the original instance with an instance of the custom class.
+        add_action( 'init', 'replace_wp_travel_cart_instance', 1 );
+        
+    function replace_wp_travel_cart_instance() {
+            global $wt_cart;
+            // Set $wt_cart to an instance of the custom class to override methods. 
+            $wt_cart = new Custom_WP_Travel_Cart();
         }
     }
-
-    // Replace the original instance of WP_Travel_Cart with the custom instance
-    add_action( 'plugins_loaded', 'replace_WP_Travel_Cart_instance', 10 );
-    function replace_WP_Travel_Cart_instance() {
-        global $wp_travel_cart;
-        $wp_travel_cart = new Custom_WP_Travel_Cart(); // Instantiate the custom class
-    }
-}
+    
+    
+    
