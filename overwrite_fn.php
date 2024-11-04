1352f97b75cbecb9cd55c5bc4cb71bf116ad3fde
twindai <guandai@hotmail.com>
2024-11-04 19:14:34 +0100

    remove

--- overwrite_fn.php
+++ overwrite_fn.php
@@ -1,27 +1,27 @@
 <?php
 
-// Check if the original WP_Travel_Cart class exists before creating the custom class.
-if ( class_exists( 'WP_Travel_Cart' ) ) {
-        // Define a custom class that extends WP_Travel_Cart.
-        class Custom_WP_Travel_Cart extends WP_Travel_Cart {
+// // Check if the original WP_Travel_Cart class exists before creating the custom class.
+// if ( class_exists( 'WP_Travel_Cart' ) ) {
+//         // Define a custom class that extends WP_Travel_Cart.
+//         class Custom_WP_Travel_Cart extends WP_Travel_Cart {
     
-            // Override the cart_empty_message method with custom functionality.
-            public function cart_empty_message() {
-                $url = get_post_type_archive_link( WP_TRAVEL_POST_TYPE );
-                echo sprintf( __( 'Your cart is empty please <a href="%s"> click here </a> to add trips.', 'wp-travel' ), esc_url( $url ) ); // @phpcs:ignore
-            }
-        }
+//             // Override the cart_empty_message method with custom functionality.
+//             public function cart_empty_message() {
+//                 $url = get_post_type_archive_link( WP_TRAVEL_POST_TYPE );
+//                 echo sprintf( __( 'Your cart is empty please <a href="%s"> click here </a> to add trips.', 'wp-travel' ), esc_url( $url ) ); // @phpcs:ignore
+//             }
+//         }
     
-        // Replace the original instance with an instance of the custom class.
-        add_action( 'init', 'replace_wp_travel_cart_instance', 1 );
+//         // Replace the original instance with an instance of the custom class.
+//         add_action( 'init', 'replace_wp_travel_cart_instance', 1 );
         
-    function replace_wp_travel_cart_instance() {
-            new Custom_WP_Travel_Cart();
+//     function replace_wp_travel_cart_instance() {
+//             new Custom_WP_Travel_Cart();
 
-            // Set cart global variable.
-            $GLOBALS['wt_cart'] = new Custom_WP_Travel_Cart();
-        }
-    }
+//             // Set cart global variable.
+//             $GLOBALS['wt_cart'] = new Custom_WP_Travel_Cart();
+//         }
+//     }
     
     
     
