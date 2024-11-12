3b587b65e5bba0e898da4acaf8f9f0c4ecb9170f
twindai <guandai@hotmail.com>
2024-11-12 19:17:32 +0100

    update

--- js/form_change_upload_file.js
+++ js/form_change_upload_file.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
 	setTimeout(()=> {
-		var fileInput = document.getElementById("file-plan");
+		var fileInputs = document.querySelectorAll(".file-plan");
 
 		// Create a custom label button
 		var label = document.createElement("label");
@@ -15,22 +15,25 @@ document.addEventListener('DOMContentLoaded', function () {
 		label.style.borderRadius = "5px";
 		label.style.cursor = "pointer";
 		
-		// Hide the original file input
-		fileInput.style.display = "none";
-		
-		// Add event listener to open file dialog when label is clicked
-		label.addEventListener("click", function() {
-			fileInput.click();
+		fileInputs.forEach(input => {
+			// Hide the original file input
+			fileInput.style.display = "none";
+			
+			// Add event listener to open file dialog when label is clicked
+			label.addEventListener("click", function() {
+				fileInput.click();
+			});
+			
+			// Append the label next to the input field
+			fileInput.parentNode.insertBefore(label, fileInput.nextSibling);
+			
+			// Optionally, display selected file name
+			fileInput.addEventListener("change", function() {
+		   var fileName = fileInput.files.length > 0 ? fileInput.files[0].name : "空";
+		   label.innerHTML = fileName || fileName !== "空" ? `文件已选择: ${fileName}`  : "上传行程文件";
+			});
 		});
 		
-		// Append the label next to the input field
-		fileInput.parentNode.insertBefore(label, fileInput.nextSibling);
-		
-		// Optionally, display selected file name
-		fileInput.addEventListener("change", function() {
-	   var fileName = fileInput.files.length > 0 ? fileInput.files[0].name : "空";
-	   label.innerHTML = fileName || fileName !== "空" ? `文件已选择: ${fileName}`  : "上传行程文件";
-		});
 			
 	}, 1000);
 });
