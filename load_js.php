9ab385a3d307d06490c46ab1e6b80bde2c8c36ce
twindai <guandai@hotmail.com>
2024-11-13 18:59:06 +0100

    hide_anonymouse_download

--- load_js.php
+++ load_js.php
@@ -33,4 +33,5 @@ insert_js_by_url_name('form_show_sent_modal', '/book_');
 insert_js_by_url_name('global_navigation', '/');
 insert_js_by_url_name('account_order_data', '/account');
 insert_js_by_url_name('itinerary_booking_btn', '/itinerary');
+insert_js_by_url_name('hide_anonymouse_download', '/itinerary');
 
