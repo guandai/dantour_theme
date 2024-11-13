095b6e09b6a2b952b3233d4e3fa74b3ce58226c3
twindai <guandai@hotmail.com>
2024-11-13 23:45:38 +0100

    car-type-box

--- js/form_vehicel_car_pic.js
+++ js/form_vehicel_car_pic.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
 	setTimeout(() => {
-		const car_type = document.querySelector('#car_type');
-		if (!car_type) {
+		const car_type = document.querySelectorAll('.wpcf7-form-control.wpcf7-radio.car_type');
+		if (car_type.length === 0) {
 			return false;
 		}
 
@@ -11,14 +11,16 @@ document.addEventListener('DOMContentLoaded', function () {
 		  "小型巴士": "https://dantourbooking.com/wp-content/uploads/2024/09/sprinter-2016.png",
 		  "大型巴士": "https://dantourbooking.com/wp-content/uploads/2024/09/duesseldorf-vip-bus-freigestellt-1536x792-1.png",
 		};
-		car_type.childNodes.forEach((node) => {
-			node.classList.add('car-type-box');
-			const type = node.querySelector('input[name=car_type]');
-			const img_src = car_map[type.value];
-			const img = document.createElement('img');
-			img.src = img_src;
-			img.className = 'car-type-img';
-			node.insertBefore(img, node.firstChild);
+		car_type.forEach((car_node) => {
+			car_node.childNodes.forEach((node) => {
+				node.classList.add('car-type-box');
+				const type = node.querySelector('input[name=car_type]');
+				const img_src = car_map[type.value];
+				const img = document.createElement('img');
+				img.src = img_src;
+				img.className = 'car-type-img';
+				node.insertBefore(img, node.firstChild);
+			});
 		});
 	}, 1000);	
 });
