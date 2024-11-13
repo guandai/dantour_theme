b5cd1855bd0b150bce1f2f1efd2dc211e9ce7c5b
twindai <guandai@hotmail.com>
2024-11-13 20:51:08 +0100

    remove later

--- js/hide_anonymouse_download.js
+++ js/hide_anonymouse_download.js
@@ -3,8 +3,7 @@ document.addEventListener('DOMContentLoaded', function() {
 		// detect wpUserData exist or not,   if  exist remove  #downloads  element
 		let downloads = document.getElementById('downloads');
 		if ( window.wpUserData === undefined ) {
-			downloads.remove();
-		} else {	
+			
 			// add a button  at center for login to /login page
 			let loginBtn = document.createElement('button');
 			loginBtn.innerText = "登录";
@@ -16,6 +15,7 @@ document.addEventListener('DOMContentLoaded', function() {
 			});
 			const sliderTab = document.querySelector('#slider-tab');
 			sliderTab.insertAdjacentElement('afterend', loginBtn);
+			downloads.remove();
 		}
 	}, 1000);
 });
