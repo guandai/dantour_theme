2522a8ed04562d0566f8ca7e8ba3c58ce711b101
twindai <guandai@hotmail.com>
2024-11-13 20:58:47 +0100

    className

--- js/hide_anonymouse_download.js
+++ js/hide_anonymouse_download.js
@@ -1,13 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
 	setTimeout(() => {
-		// detect wpUserData exist or not,   if  exist remove  #downloads  element
 		let downloads = document.getElementById('downloads');
 		if ( window.wpUserData === undefined ) {
-			
 			// add a button  at center for login to /login page
 			let loginBtn = document.createElement('button');
 			let loginDiv = document.createElement('div');
-			loginDiv.class="btn btn-center-login";
+			loginDiv.className = "login-div";
 			loginBtn.innerText = "登录";
 			loginBtn.style.position = "absolute";
 
